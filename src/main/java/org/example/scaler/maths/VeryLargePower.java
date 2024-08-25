4a03d3bf77e04d424593f017889f3e037f005dd5
Durga Prasad Surada <durgaprasad2004@gmail.com>
2024-08-25 09:20:51 +0530

    Very Large Power

---
+++ src/main/java/org/example/scaler/maths/VeryLargePower.java
@@ -0,0 +1,18 @@
+package org.example.scaler.maths;
+
+/**
+ * Given two Integers A, B. You have to calculate (A ^ (B!)) % (1e9 + 7).
+ *
+ * "^" means power,
+ * "%" means mod, and
+ * "!" means factorial.
+ *
+ * Note: Ensure to handle integer overflow when performing the calculations.
+ *
+ *
+ * Problem Constraints
+ * 1 <= A, B <= 5e5
+ */
+public class VeryLargePower {
+
+}
